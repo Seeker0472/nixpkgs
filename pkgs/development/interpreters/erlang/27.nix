cd3730663d906d51c7692a2f9762279bab6403dc
seeker <gmx472@qq.com>
2025-06-15 16:34:57 +0800

    remove all

--- pkgs/development/interpreters/erlang/27.nix
+++
@@ -1,6 +0,0 @@
-{ mkDerivation }:
-
-mkDerivation {
-  version = "27.3.4";
-  sha256 = "sha256-RZiN17k5fSIQUYxOoAts5iy/obgoPM0RFB5YXAfkTSk=";
-}
--- pkgs/development/libraries/itk/5.x.nix
+++
@@ -1,5 +0,0 @@
-import ./generic.nix rec {
-  version = "5.4.3";
-  tag = "v${version}";
-  sourceSha256 = "sha256-Ve9AzgzePYb6mJ6OZ6C4YeiggCd4WBxB4Xu3ju5HhAg=";
-}
